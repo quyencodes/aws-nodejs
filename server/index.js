1ddd4676c15d943d7ea7e6a405af3f0e473c08ef
Quyen Hoang <qduhoang@gmail.com>
2023-05-16 15:21:56 -0700

    Completed src code for server/index.js

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express()
+const NAME = 'quyenhoang'
+
+app.get('/', (req, res) => {
+  res.status(200).json('This test route is working just fine')
+})
+
+app.get(`/${NAME}`, (req, res) => {
+  res.status(200).json(`Request to ${NAME} route successful`)
+})
+
+app.post('/', (req, res) => {
+  const query = req.body
+  if (!query.user) {
+    res.status(400).json('Create request unsuccessful, please send a JSON body with a user property')
+  } else {
+    res.status(201).json(`Request to create ${query.user} successful`)
+  }
+})
+
+app.put('/:id', (req, res) => {
+  const query = req.params
+  if (!query.id) {
+    res.status(400).json('Update request unsuccessful, please include a fake id in the address path')
+  } else {
+    res.status(200).json(`Request to update ${query.id} successful`)
+  }
+})
+
+app.delete('/:id', (req, res) => {
+  const query = req.params
+  if (!query.id) {
+    res.status(400).json('Delete request unsuccessful, please include a fake id in the address path')
+  } else {
+    res.status(200).json(`Request to delete ${query.id} successful`)
+  }
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
